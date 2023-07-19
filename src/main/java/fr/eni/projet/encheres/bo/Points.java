99ce5bc474d7f0d9525105d0509c9072fe6a00e2
Nabil <nabil.sayad2023@campus-eni.fr>
2023-07-19 09:48:29 +0200

    Finalisation de la classe Points dans la couche BO

--- src/main/java/fr/eni/projet/encheres/bo/Points.java
+++ src/main/java/fr/eni/projet/encheres/bo/Points.java
@@ -3,7 +3,50 @@ package fr.eni.projet.encheres.bo;
 public class Points {
 	
 	private int noUtilisateur;
+	private String pseudo;
 	private int noPoints;
 	
+	
+	// Constructeur sans noPoints et noUtilisateur
+	
+	public Points(String pseudo) {
+		this.pseudo = pseudo;
+	}
+	
+	// Constructeur 
+	
+	public Points(int noUtilisateur, String pseudo, int noPoints) {
+		this.noUtilisateur = noUtilisateur;
+		this.pseudo = pseudo;
+		this.noPoints = noPoints;
+	}
+	
+	// Getter & Setter
+	
+	public int getNoUtilisateur() {
+		return noUtilisateur;
+	}
+	public void setNoUtilisateur(int noUtilisateur) {
+		this.noUtilisateur = noUtilisateur;
+	}
+	public String getPseudo() {
+		return pseudo;
+	}
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
+	public int getNoPoints() {
+		return noPoints;
+	}
+	public void setNoPoints(int noPoints) {
+		this.noPoints = noPoints;
+	}
+	
+	// Methode toString
+	
+	@Override
+	public String toString() {
+		return "Points [noUtilisateur=" + noUtilisateur + ", pseudo=" + pseudo + ", noPoints=" + noPoints + "]";
+	}
 
 }
