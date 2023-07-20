18894461ae5a9e802f7841b1dc8d336ae41eb80e
nicobdc44 <nbdcfr@hotmail.com>
2023-07-20 15:18:55 +0200

    .

--- src/main/java/fr/eni/projet/encheres/bll/UtilisateurManager.java
+++ src/main/java/fr/eni/projet/encheres/bll/UtilisateurManager.java
@@ -15,32 +15,24 @@ package fr.eni.projet.encheres.bll;
 	    }
 
 	    // Méthode pour l'inscription d'un nouvel utilisateur
-<<<<<<< HEAD
-	    public void sinscrire (String pseudo, String nom, String prenom, String email, String telephone, String rue, String codePostal, String ville, String motDePasse, String credit, String administrateur) throws BusinessException {
-=======
-	    public void sinscrire (String pseudo, String motDePasse, String email, String prenom, int codePostal, String ville, String adresse) throws BusinessException {
->>>>>>> 76e080fb7fb3c6118b579cab34092fa9dba2ed93
+
+
+	    public void sinscrire (String pseudo, String nom, String prenom, String email,, int codePostal, String ville, String adresse) throws BusinessException {
 	        try {
 	        	Utilisateur u = new Utilisateur();
 	        	u.setPseudo(pseudo);
-	        	u.setMotDePasse(nom);
-	        	u.setEmail(prenom);
-	        	u.setPrenom(email);
+	        	u.setNom(nom);
+	        	u.setPrenom(prenom);
 	        	u.setPseudo(telephone);
 	        	u.setMotDePasse(rue);
 	        	u.setEmail(codePostal);
 	        	u.setPrenom(ville);
 	        	u.setMotDePasse(motDePasse);
-<<<<<<< HEAD
 	        	u.setEmail(credit);
 	        	u.setPrenom(administrateur);
-=======
 	        	u.setEmail(email);
 	        	u.setPrenom(prenom);
 	        	u.setCodePostal(codePostal);
-	        	u.setVille(ville);
-	        	u.setAdresse(adresse);
->>>>>>> 76e080fb7fb3c6118b579cab34092fa9dba2ed93
 	        	
 	        	//validateUtilisateur(utilisateur, motDePasse);
 	            DAOUtilisateur.ajouterUtilisateur(u);
