4b9f7538d2af33b0222d1bab64356cb8205d17c2
nicobdc44 <nbdcfr@hotmail.com>
2023-07-25 16:00:54 +0200

    creation page modif compte

--- src/main/java/fr/eni/projet/encheres/ihm/PageModifierProfil.java
+++ src/main/java/fr/eni/projet/encheres/ihm/PageModifierProfil.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import fr.eni.projet.encheres.BusinessException;
+import fr.eni.projet.encheres.bll.UtilisateurManager;
 import fr.eni.projet.encheres.bo.Utilisateur;
 
 /**
@@ -31,6 +33,8 @@ public class PageModifierProfil extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession();
 		Integer noUtilisateur = (Integer) session.getAttribute("identifiant");
+		System.out.println(noUtilisateur);
+		System.out.println("identifiant");
         if (noUtilisateur == null) {
             response.sendRedirect("/PageConnexion");
             return;
@@ -57,22 +61,30 @@ public class PageModifierProfil extends HttpServlet {
         String email = request.getParameter("email");
         String telephone = request.getParameter("telephone");
         String rue = request.getParameter("rue");
-        int codePostal = request.getParameter("codePostal");
+        int codePostal = Integer.parseInt(request.getParameter("codePostal"));
         String ville = request.getParameter("ville");
         String motDePasse = request.getParameter("motDePasse");
         String pseudo = request.getParameter("pseudo");
 
         // Mettre à jour les informations de l'utilisateur
-        utilisateur.setPseudo("pseudo");
-        utilisateur.setNom("nom");
-        utilisateur.setPrenom("prenom");
-        utilisateur.setEmail("email");
-        utilisateur.setTelephone("telephone");
-        utilisateur.setRue("rue");
-        utilisateur.setCodePostal("codePostal");
-        utilisateur.setVille("ville");
-        utilisateur.setMotDePasse("motDePasse");
+       /* utilisateur.setPseudo(pseudo);
+        utilisateur.setNom(nom);
+        utilisateur.setPrenom(prenom);
+        utilisateur.setEmail(email);
+        utilisateur.setTelephone(telephone);
+        utilisateur.setRue(rue);
+        utilisateur.setCodePostal(codePostal);
+        utilisateur.setVille(ville);
+        utilisateur.setMotDePasse(motDePasse);*/
         
+        UtilisateurManager utilisateurManager = new UtilisateurManager();
+        //Utilisateur utilisateur = null;
+        try {
+			utilisateurManager.mettreAJourUtilisateurProfil(utilisateur,pseudo,motDePasse,nom,prenom,email,telephone,rue,codePostal,ville);
+		} catch (BusinessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
         response.sendRedirect("Enchere-Eni/PageModifierProfil");
         
 		doGet(request, response);
