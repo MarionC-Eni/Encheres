5b50657aa6323ad3605f836bd389cf924bf20a2d
Nabil <nabil.sayad2023@campus-eni.fr>
2023-07-25 10:18:42 +0200

    Servlet et Jsp modifierProfil, utilisateur manager - methode modifier profil

--- src/main/java/fr/eni/projet/encheres/ihm/PageModifierProfil.java
+++ src/main/java/fr/eni/projet/encheres/ihm/PageModifierProfil.java
@@ -6,6 +6,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import fr.eni.projet.encheres.bo.Utilisateur;
 
 /**
  * Servlet implementation class PageModifierProfil
@@ -26,7 +29,12 @@ public class PageModifierProfil extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
+        Utilisateur utilisateur = (Utilisateur) session.getAttribute("utilisateur");
+        if (utilisateur == null) {
+            response.sendRedirect("Enchere-Eni/PageModifierProfil");
+            return;
+        }
 		this.getServletContext().getRequestDispatcher("/html/PageModifierProfil.jsp").forward(request, response);
 	}
 
@@ -34,7 +42,39 @@ public class PageModifierProfil extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
+        Utilisateur utilisateur = (Utilisateur) session.getAttribute("utilisateur");
+
+        if (utilisateur == null) {
+            // Si l'utilisateur n'est pas connecté, rediriger vers la page de connexion
+            response.sendRedirect("/PageModifierProfil");
+            return;
+        }
+
+        // Récupérer les données du formulaire de modification
+        String nom = request.getParameter("nom");
+        String prenom = request.getParameter("prenom");
+        String email = request.getParameter("email");
+        String telephone = request.getParameter("telephone");
+        String rue = request.getParameter("rue");
+        String codePostal = request.getParameter("codePostal");
+        String ville = request.getParameter("ville");
+        String motDePasse = request.getParameter("motDePasse");
+        String pseudo = request.getParameter("pseudo");
+
+        // Mettre à jour les informations de l'utilisateur
+        utilisateur.setPseudo("pseudo");
+        utilisateur.setNom("nom");
+        utilisateur.setPrenom("prenom");
+        utilisateur.setEmail("email");
+        utilisateur.setTelephone("telephone");
+        utilisateur.setRue("rue");
+        utilisateur.setCodePostal("codePostal");
+        utilisateur.setVille("ville");
+        utilisateur.setMotDePasse("motDePasse");
+        
+        response.sendRedirect("Enchere-Eni/PageModifierProfil");
+        
 		doGet(request, response);
 	}
 
--- src/main/webapp/html/PageModifierProfil.jsp
+++ src/main/webapp/html/PageModifierProfil.jsp
@@ -8,5 +8,27 @@
 </head>
 <body>
 <h1>PageModifierProfil</h1>
+<form method="POST" action="/Enchere-Eni/PageModifierProfil">
+<label for="pseudo">pseudo :</label>
+<input type="text" id="pseudo" name="pseudo" required>
+<label for="nom">Nom :</label>
+<input type="text" id="nom" name="nom" required>
+<label for="Prenom">Prénom :</label>
+<input type="text" id="Prenom" name="prenom" required>
+<label for="email">Email :</label>
+<input type="email" id="email" name="email" maxlength= "40" required>
+<label for="telephone">Téléphone :</label>
+<input type="tel" id="telephone" name="telephone" maxlength= "10" required>
+<label for="rue">Rue :</label>
+<input type="text" id="rue" name="rue" required>
+<label for="codePostal">Code postal :</label>
+<input type="number" id="codePostal" name="codePostal" maxlength= "5" required>
+<label for="ville">Ville :</label>
+<input type="text" id="ville" name="ville" maxlength= "25" required>
+<label for="motDePasse">Mot de passe :</label>
+<input type="password" id="motDePasse" name="motDePasse" required>
+<input type="submit" value="Créer">
+<a href="/Enchere-Eni/PagesListeEncheresConnecte"><button>Annuler</button></a>
+</form>
 </body>
 </html>
